3ff8771a7a539f29a39e71892cd8f766ebbb3b70
Zhaburonok Viktor <viktor.zhaburonok@gmail.com>
2024-03-30 11:34:51 +0300

    add new line

--- go/app.go
+++ go/app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, 世界")
-	fmt.Fprintln(w, "Hello, 世界")
+	fmt.Fprintln(w, "Hello, 世界 Hello, 世界")
 }
 
 func main() {
