b3e0fb689acfd1bf30291e3e87665add198b49d8
Zhaburonok Viktor <viktor.zhaburonok@gmail.com>
2024-05-11 10:09:29 +0300

    Update app.go

--- go/app.go
+++ go/app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, 世界")
-	fmt.Fprintln(w, "This line is added as feature, to test CI/CD process")
+	fmt.Fprintln(w, "Hello, 世界")
 	fmt.Fprintln(w, "This line is added as feature, to test CI/CD process")
 }
 
